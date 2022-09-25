fe054acf0d7b1d63b733046cdc2e9aa20a590426
Mahulicious <maheshdhimal61@gmail.com>
2022-09-25 11:49:09 -0400

    Finalized the playRound function.

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 // Creating a game of rock, paper, scissors that will be played in console.
 
 
-const value = ["Rock", "Paper", "Scissors"];
+const value = ["ROCK", "PAPER", "SCISSORS"];
+
 
 
 function getComputerChoice (value) {
@@ -12,25 +13,55 @@ function getComputerChoice (value) {
 }
 getComputerChoice(value);
 
-function playRound (playerChoice, computerChoice ) {
-    if (playerChoice == "Rock" && computerChoice == "Paper") {
+function getPlayerChoice () {
+    let choice = prompt("Please enter Rock, Paper or Scissors");
+    let player= choice.toLocaleUpperCase();
+    return player;
+
+}
+
+const computerChoice = getComputerChoice(value);
+const playerChoice = getPlayerChoice();
+
+function playRound (computerChoice, playerChoice ) {
+    let computerScore;
+    let playerScore;
+    if (computerChoice == "PAPER" && playerChoice == "ROCK") {
+        computerScore + 1;
         return console.log("You lose! Paper beats Rock!");
-    } else if (playerChoice == "Scissors" && computerChoice == "Rock") {
+    } else if (computerChoice == "ROCK" && playerChoice == "SCISSORS") {
+        computerScore + 1;
         return console.log("You lose! Rock beats Scissors!");
-    } else if (playerChoice == "Paper" && computerChoice == "Scissors") {
+    } else if (computerChoice == "SCISSORS" && playerChoice== "PAPER") {
+        computerScore + 1;
         return console.log("You lose! Scissors beats Paper!");
-    } else if (playerChoice == computerChoice) {
+    } else if (computerChoice == playerChoice) {
         return console.log("The game is a draw!");
     } else {
+        playerScore + 1;
         return console.log("You win!");
     }
 
 
 }
-const computerChoice = getComputerChoice(value);
-let choice = prompt("Please enter Rock, Paper or Scissors");
-let playerChoice = choice.toUpperCase();
-console.log(playRound(computerChoice, playerChoice));
+
+
+
+
+
+function game () {    
+   let computerScore = 0;
+   let playerScore = 0;
+for (let i = 0; i < 5; i++ ) {
+   playRound(computerChoice, playerChoice);
+   console.log(playRound);
+    
+
+
+
+}
+}
+game();
 
 
 
